f474550546599dd182622bf3f66507c00381d8b8
david.cc <david.cc@dataspartan.com>
2024-01-30 12:54:06 +0100

    poc

--- app/lib/timelog_controller_patch.rb
+++ app/lib/timelog_controller_patch.rb
@@ -54,7 +54,8 @@ module TimelogControllerPatch
 				format.csv do
 					# Export all entries
 					@entries = scope.to_a
-					send_data(query_to_csv(@entries, @query, params), :type => 'text/csv; header=present', :filename => "#{filename_for_export(@query, 'timelog')}.csv")
+					# send_data(query_to_csv(@entries, @query, params), :type => 'text/csv; header=present', :filename => "#{filename_for_export(@query, 'timelog')}.csv")
+					send_data(query_to_csv(@entries, @query, params), :type => 'text/csv; header=present', :filename => "data.csv")
 				end
 			end
 		end
